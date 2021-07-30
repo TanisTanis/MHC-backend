10b5f65006e6859854a5c1c06ff05b7724ea446e
Tanis Kiel <tkiel12@gmail.com>
2021-07-29 20:58:55 -0700

    Confirmation token endpoints working

---
+++ src/main/java/com/mentally/mentalhealthcalendar/registration/RegistrationController.java
@@ -0,0 +1,24 @@
+package com.mentally.mentalhealthcalendar.registration;
+
+import com.mentally.mentalhealthcalendar.service.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="api/auth/signup")
+@AllArgsConstructor
+public class RegistrationController {
+    private final RegistrationService registrationService;
+    private final AppUserService appUserService;
+
+    @PostMapping
+    public Object signup(@RequestBody RegistrationRequest newUser) throws Throwable {
+        return registrationService.register(newUser);
+    }
+
+    @GetMapping(path = "/confirm")
+    public String confirmToken(@RequestParam("token") String token) {
+        return registrationService.confirmToken(token);
+    }
+}
